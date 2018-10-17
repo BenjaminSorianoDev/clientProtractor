c6c1e96af056b5c93acb0ff7d3e13807a4e0f276
Benjamin SORIANO <soriano.benjamin.pro@gmail.com>
2018-10-17 17:05:08 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
